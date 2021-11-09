20f2c19be21a2b4a012d414f5206a37f9b4fd62a
Ellen Guo <elleng18@vt.edu>
2021-11-09 15:04:04 -0500

    not much update

--- cURL2.py
+++ cURL2.py
@@ -14,13 +14,27 @@ def verify_password(username, password):
     collection = db["service_auth"]
     name = user.get(username)
     secret = user.get(password)
+    print(name, secret) #check and see if username and password is returned
+
+@auth.error_handler
+def auth_error(status):
+    return "Access Denied: ", status
 
 @app.route('/LED')
+@auth.login_required
 def LED():
     command = request.args.get('command')
     status = command[0:command.find('-')]
     color = command[command.find('-') + 1: command.find('-', command.find('-') + 1)]
     intensity = command[command.find('-', command.find('-') + 1) + 1:]
+
+@app.route('/Canvas')
+@auth.login_required
+def canvas_API():
+    # grab filename
+    filename = request.args.get('file')
+    # turn into a Python HTTP Request
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081, debug=True)
