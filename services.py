c513c6cc07e49954d825704d474102790d5c6c27
Ellen-Guo <60410327+Ellen-Guo@users.noreply.github.com>
2021-11-14 17:39:37 -0500

    Final???

--- services.py
+++ services.py
@@ -21,17 +21,6 @@ global ip, port
 ip = None
 port = None
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-
 def signal_handler(signal, frame):
     print("Interrupt called")
     zeroconf.unregister_service(info)
