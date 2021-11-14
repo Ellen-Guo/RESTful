76242264baef6921012197a67eac419afb34fff3
aliciaharman <60333951+aliciaharman@users.noreply.github.com>
2021-11-13 20:31:23 -0500

    changed duty cycle

--- led.py
+++ led.py
@@ -22,14 +22,78 @@ def LED():
     status = request.args.get('status')
     color = request.args.get('color')
     intensity = request.args.get('intensity')
+
+    r = 27
+    g = 13
+    b = 26
+
+    # Setup GPIO
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    LEDS = (r, g, b)
+    GPIO.setup(LEDS, GPIO.OUT)
+    GPIO.output(LEDS, False)
+
+    pR = GPIO.PWM(r, 50)  # frequency=50Hz
+    pG = GPIO.PWM(g, 50)  # frequency=50Hz
+    pB = GPIO.PWM(b, 50)  # frequency=50Hz
+    
+    changeLED(intensity, color, status)
+    
+    def changeDC(iR, iG, iB):
+        pR.start(iR)
+        pG.start(iG)
+        pB.start(iB)
+
+    def changeLED(intensity, color, status):
+        if status == 'on':
+            if color == 'white':
+                changeDC(intensity, intensity, intensity)
+                GPIO.output(LEDS, (GPIO.HIGH, GPIO.HIGH, GPIO.HIGH))
+            elif color == 'red':
+                changeDC(intensity, 0, 0)
+                GPIO.output(LEDS, (GPIO.HIGH, GPIO.LOW, GPIO.LOW))
+            elif color == 'blue':
+                changeDC(0, 0, intensity)
+                GPIO.output(LEDS, (GPIO.LOW, GPIO.LOW, GPIO.HIGH))
+            elif color == 'green':
+                changeDC(0, intensity, 0)
+                GPIO.output(LEDS, (GPIO.LOW, GPIO.HIGH, GPIO.LOW))
+            elif color == 'magenta':
+                changeDC(intensity, 0, intensity)
+                GPIO.output(LEDS, (GPIO.HIGH, GPIO.LOW, GPIO.HIGH))
+            elif color == 'cyan':
+                changeDC(0, intensity, intensity)
+                GPIO.output(LEDS, (GPIO.LOW, GPIO.HIGH, GPIO.HIGH))
+            elif color == 'yellow':
+                changeDC(intensity, intensity, 0)
+                GPIO.output(LEDS, (GPIO.HIGH, GPIO.HIGH, GPIO.LOW))
+        else:
+            pR.stop()
+            pG.stop()
+            pB.stop()
+            GPIO.output(LEDS, False)
+    
+    
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Unregistering...")
+        zeroconf.unregister_service(info)
+        zeroconf.close()
     return "status: %s. Color: %s. Intensity: %s" % (status, color, intensity)
 
 
 desc = {'Version': '1.0'}
+local = socket.gethostbyname(socket.gethostname() + ".local")
+print(local)
 info = ServiceInfo(
     "_http._tcp.local.",
     "Testing._http._tcp.local.",
-    addresses= [socket.inet_aton("0.0.0.0")], #Need IP address of LED pi. For testing on same device, use 0.0.0.0
+    addresses= [socket.inet_aton(local)], #Need IP address of LED pi. For testing on same device, use 0.0.0.0
     port=5000,
     properties=desc
 #         server=socket.gethostname() + '.local.',
@@ -47,86 +111,5 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 
-if __name__ == '__main__':
-    
-    
-    
+if __name__ == '__main__':    
     app.run(host="0.0.0.0", port=5000, debug=True)
-    
-    
-    r = 27
-    g = 13
-    b = 26
-
-    # Setup GPIO
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    LEDS = (r, g, b)
-    GPIO.setup(LEDS, GPIO.OUT)
-    GPIO.output(LEDS, False)
-
-    pR = GPIO.PWM(r, 50)  # frequency=50Hz
-    pR.start(0)
-    pG = GPIO.PWM(g, 50)  # frequency=50Hz
-    pG.start(0)
-    pB = GPIO.PWM(b, 50)  # frequency=50Hz
-    pB.start(0)
-    
-    changeLED(intensity, color, status)
-
-    # Reset pins
-    pR.stop()
-    pG.stop()
-    pB.stop()
-    GPIO.cleanup()
-
-    def changeLED(intensity, color, status):
-        if status == 'on':
-            if color == 'white':
-                pR.ChangeDutyCycle(intensity)
-                pG.ChangeDutyCycle(intensity)
-                pB.ChangeDutyCycle(intensity)
-                GPIO.output(LEDS, (GPIO.HIGH, GPIO.HIGH, GPIO.HIGH))
-            elif color == 'red':
-                pR.ChangeDutyCycle(intensity)
-                pG.ChangeDutyCycle(0)
-                pB.ChangeDutyCycle(0)
-                GPIO.output(LEDS, (GPIO.HIGH, GPIO.LOW, GPIO.LOW))
-            elif color == 'blue':
-                pR.ChangeDutyCycle(0)
-                pG.ChangeDutyCycle(0)
-                pB.ChangeDutyCycle(intensity)
-                GPIO.output(LEDS, (GPIO.LOW, GPIO.LOW, GPIO.HIGH))
-            elif color == 'green':
-                pR.ChangeDutyCycle(0)
-                pG.ChangeDutyCycle(intensity)
-                pB.ChangeDutyCycle(0)
-                GPIO.output(LEDS, (GPIO.LOW, GPIO.HIGH, GPIO.LOW))
-            elif color == 'magenta':
-                pR.ChangeDutyCycle(intensity)
-                pG.ChangeDutyCycle(0)
-                pB.ChangeDutyCycle(intensity)
-                GPIO.output(LEDS, (GPIO.HIGH, GPIO.LOW, GPIO.HIGH))
-            elif color == 'cyan':
-                pR.ChangeDutyCycle(0)
-                pG.ChangeDutyCycle(intensity)
-                pB.ChangeDutyCycle(intensity)
-                GPIO.output(LEDS, (GPIO.LOW, GPIO.HIGH, GPIO.HIGH))
-            elif color == 'yellow':
-                pR.ChangeDutyCycle(intensity)
-                pG.ChangeDutyCycle(intensity)
-                pB.ChangeDutyCycle(0)
-                GPIO.output(LEDS, (GPIO.HIGH, GPIO.HIGH, GPIO.LOW))
-        else:
-            GPIO.output(LEDS, False)
-    
-    
-    try:
-        while True:
-            sleep(0.1)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("Unregistering...")
-        zeroconf.unregister_service(info)
-        zeroconf.close()
